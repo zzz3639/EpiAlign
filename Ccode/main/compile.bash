cbc364ddff90c13e2f9279fd2b67786aac0a4d56
zzz <zzz@zzz-Inspiron-3420.(none)>
2018-08-10 08:40:11 +0800

    manual updated

--- Ccode/main/compile.bash
+++ Ccode/main/compile.bash
@@ -4,7 +4,7 @@ gcc cut_sseq.c WatermanFun.c StateIO.c -O3 -o ../bin/cut_sseq.out
 gcc DatabaseSearch.c WatermanFun.c StateIO.c CustomFunction.c -O3 -o ../bin/DatabaseSearch.out
 gcc DatabaseSearchNative.c WatermanFun.c StateIO.c CustomFunction.c -O3 -o ../bin/DatabaseSearchNative.out
 gcc Algn2Ano.c StateIO.c -O3 -o ../bin/Algn2Ano.out
-g++-5 -std=c++11 FakeChromosomeGenerator.cpp WatermanFun.c StateIO.c -O3 -o ../bin/FakeChromosomeGenerator.out
+g++ -std=c++11 FakeChromosomeGenerator.cpp WatermanFun.c StateIO.c -O3 -o ../bin/FakeChromosomeGenerator.out
 gcc -O3 DatabaseSearch_baseline1.c WatermanFun.c StateIO.c -lm -o ../bin/DatabaseSearch_baseline1.out
 gcc -O3 DatabaseSearch_baseline2.c WatermanFun.c StateIO.c -lm -o ../bin/DatabaseSearch_baseline2.out
 gcc -O3 DatabaseSearch_baseline3.c WatermanFun.c StateIO.c -lm -o ../bin/DatabaseSearch_baseline.out
---
+++ README.txt
@@ -0,0 +1,3 @@
+Downloading zip file directly may cause errors in links.
+
+if an error occurs,  we recommend you to download our package by "git clone" ruther than downloading the zip file.
